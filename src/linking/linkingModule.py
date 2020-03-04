d8c316ba3bb0ba6b1b1d7dbe6c861a7d191cc98b
Luca Foppiano <luca@foppiano.org>
2020-03-04 13:53:15 +0900

    cleanup

--- src/linking/linkingModule.py
+++ src/linking/linkingModule.py
@@ -1,146 +1,20 @@
 import copy
-import csv
-import json
-import ntpath
-import os
-import sys
-from pathlib import Path
-from sys import argv
 
-import requests
 import spacy
 from gensim.summarization.textcleaner import split_sentences
-from src.linking.relationships_resolver import SimpleResolutionResolver, VicinityResolutionResolver, \
-    DependencyParserResolutionResolver
-# from material_parser.material_parser import MaterialParser
+from material_parser.material_parser import MaterialParser
 from spacy.tokens import Span, Doc
 
+from src.linking.relationships_resolver import SimpleResolutionResolver, VicinityResolutionResolver, \
+    DependencyParserResolutionResolver
+
 nlp = spacy.load("en_core_web_sm", disable=['ner'])
-
-# mp = MaterialParser(verbose=False, pubchem_lookup=False, fails_log=False)
-
-
-def process_dir(path, out_path):
-    # xdir = Path('/data/workspace/Suzuki/supercon_files_20190808/iop/data/')
-    # xmlfiles = [x for x in xdir.glob('**/*.xml')]
-    # pdffiles = [x for x in xdir.glob('**/*.pdf')]
-    first = True
-    with open(out_path.joinpath('output_complete.json'), 'a') as f:
-        f.write("[\n")
-        for root, dirs, files in os.walk(path):
-            for file_ in files:
-                if not file_.lower().endswith(".pdf"):
-                    continue
-                abs_path = os.path.join(root, file_)
-
-                try:
-                    output_data = process_file(abs_path)
-                    if output_data['paragraphs']:
-                        if first:
-                            first = False
-                        else:
-                            f.write(",")
-                        f.write(json.dumps(output_data) + "\n")
-                        f.flush()
-
-                        for para in output_data['paragraphs']:
-                            for sent in para:
-                                relationship = list(set(sent.keys()) &
-                                                    set(['simple_relationship',
-                                                         'dep_relationship',
-                                                         'vicinity_relationship']))
-                                if len(relationship) > 0:
-                                    with open(out_path.joinpath('output_supercon.csv'), 'a') as csv_f:
-                                        writer = csv.writer(csv_f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-                                        if 'dep_relationship' in sent.keys():
-                                            for rel in sent['dep_relationship']:
-                                                writer.writerow([rel[0], rel[1], abs_path])
-                                        elif 'vicinity_relationship' in sent.keys():
-                                            for rel in sent['vicinity_relationship']:
-                                                writer.writerow([rel[0], rel[1], abs_path])
-                                        elif 'simple_relationship' in sent.keys():
-                                            for rel in sent['simple_relationship']:
-                                                writer.writerow([rel[0], rel[1], abs_path])
-
-                                if 'entities' in sent:
-                                    materials = [entity[0] for entity in sent['entities'] if
-                                                 entity[1] == 'material']
-
-                                    for material in materials:
-                                        with open(out_path.joinpath('output_materials.csv'), 'a') as csv_materials_f:
-                                            writer = csv.writer(csv_materials_f, delimiter=',', quotechar='"',
-                                                                quoting=csv.QUOTE_ALL)
-                                            writer.writerow([material])
-
-                                    temperatures = [entity[0] for entity in sent['entities'] if
-                                                    entity[1].startswith('temperature')]
-
-                                    for temperature in temperatures:
-                                        with open(out_path.joinpath('output_temperatures.csv'),
-                                                  'a') as csv_temperatures_f:
-                                            writer = csv.writer(csv_temperatures_f, delimiter=',', quotechar='"',
-                                                                quoting=csv.QUOTE_ALL)
-                                            writer.writerow([temperature])
-
-
-                except KeyboardInterrupt:
-                    print("Stop it.")
-                    if not first:
-                        f.write("]\n")
-                    sys.exit(-1)
-                except:
-                    print("Skipping file " + str(abs_path) + ", Exception: " + str(sys.exc_info()))
-
-        if not first:
-            f.write("]\n")
-
-
-def post(file_, abs_path):
-    files = {
-        'input': (
-            file_,
-            open(abs_path, 'rb'),
-            'application/pdf'
-        )
-    }
-
-    # grobid_url = "http://localhost:8072/service/processSuperconductorsPDF"
-    grobid_url = "http://falcon.nims.go.jp/superconductors/service/processSuperconductorsPDF"
-    if not grobid_url.startswith("http://localhost"):
-        os.environ['NO_PROXY'] = 'falcon.nims.go.jp'
-
-    r = requests.request(
-        "POST",
-        grobid_url,
-        headers={'Accept': 'application/json'},
-        params=None,
-        files=files,
-        data=None,
-        timeout=None
-    )
-    return r
-
 
 def decode(response):
     try:
         return response.json()
     except ValueError as e:
         return "Error: " + str(e)
-
-
-def extract_from_pdf(file_, abs_path):
-    print("Processing file " + abs_path)
-
-    r = post(file_, abs_path)
-
-    if r.status_code != 200:
-        print("Error when calling the service on file " + str(abs_path) + ", status code: " + str(r.status_code))
-        return []
-
-    jsonOut = decode(r)
-
-    return jsonOut['paragraphs']
-
 
 def convert_to_spacy2(tokens):
     outputTokens = []
@@ -152,7 +26,6 @@ def convert_to_spacy2(tokens):
 
     return outputTokens, outputSpaces
 
-
 def convert_to_spacy(tokens, spans):
     outputTokens = []
     outputSpaces = []
@@ -162,30 +35,32 @@ def convert_to_spacy(tokens, spans):
 
     newIndexOffset = 0
     entityOffset = 0
-    span = copy.copy(spans[entityOffset])
     inside = False
+    if len(spans) > 0:
+        span = copy.copy(spans[entityOffset])
 
     for index, s in enumerate(tokens):
-        if index == span['tokenStart']:
-            span['tokenStart'] = newIndexOffset
-            inside = True
-        elif index == span['tokenEnd']:
-            span['tokenEnd'] = newIndexOffset
-            outputSpans.append(span)
-            inside = False
-            if entityOffset + 1 < len(spans):
-                entityOffset += 1
-                span = copy.copy(spans[entityOffset])
-                if index == span['tokenStart']:
-                    span['tokenStart'] = newIndexOffset
-                    inside = True
-            # else:
-            #     print("finish entities")
-        elif index + 1 == len(tokens) and inside:
-            ## I'm at the last token and haven't closed the entity
-            span['tokenEnd'] = newIndexOffset
-            outputSpans.append(span)
-            inside = False
+        if len(spans) > 0:
+            if index == span['tokenStart']:
+                span['tokenStart'] = newIndexOffset
+                inside = True
+            elif index == span['tokenEnd']:
+                span['tokenEnd'] = newIndexOffset
+                outputSpans.append(span)
+                inside = False
+                if entityOffset + 1 < len(spans):
+                    entityOffset += 1
+                    span = copy.copy(spans[entityOffset])
+                    if index == span['tokenStart']:
+                        span['tokenStart'] = newIndexOffset
+                        inside = True
+                # else:
+                #     print("finish entities")
+            elif index + 1 == len(tokens) and inside:
+                ## I'm at the last token and haven't closed the entity
+                span['tokenEnd'] = newIndexOffset
+                outputSpans.append(span)
+                inside = False
 
         if skip:
             skip = False
@@ -254,7 +129,7 @@ def convert_to_spacy(tokens, spans):
         print("Something wrong in the final length check! len(outputTokens) = " + str(
             len(outputTokens)) + ", len(outputSpaces) = " + str(len(outputSpaces)))
 
-    if not len(outputSpans) == len(spans):
+    if len(spans) > 0 and not len(outputSpans) == len(spans):
         print("Something wrong in spans: len(outputSpans) = " + str(len(outputSpans)) + ", len(spans) = " + str(
             len(spans)))
 
@@ -323,6 +198,9 @@ def process_paragraph(paragraph):
 
         materials = list(filter(lambda w: w['type'] in ['material'], spans_boundary))
         temperatures = list(filter(lambda w: w['type'] in ['temperature'], spans_boundary))
+
+        ## POST-PROCESS Material names
+        # materials = post_process(materials)
 
         if len(materials) > 0 and len(temperatures) > 0:
             data_return = process_sentence(words_boundary, spaces_boundary, spans_boundary)
@@ -415,7 +293,7 @@ def markCriticalTemperature(doc):
 def process_sentence(words, spaces, spans_remapped):
     text = ''.join([words[i] + (' ' if spaces[i] else '') for i in range(0, len(words))])
 
-    print("Processing: " + text)
+    # print("Processing: " + text)
 
     ## Creating a new document with the text
     doc = Doc(nlp.vocab, words=words, spaces=spaces)
@@ -438,7 +316,7 @@ def process_sentence(words, spaces, spans_remapped):
         #     raise Exception("There is a duplicated span identifier. " + str(mapping_span_to_entity))
 
     doc.ents = entities
-    print("Entities: " + str(doc.ents))
+    # print("Entities: " + str(doc.ents))
 
     for span in entities:
         # Iterate over all spans and merge them into one token. This is done
@@ -482,8 +360,6 @@ def process_sentence(words, spaces, spans_remapped):
     # output_path.open("w", encoding="utf-8").write(svg)
 
     # extracted_entities['tokens'] = words
-
-    ## MATERIAL NAME RESOLUTION
 
     ### TC VALUES CLASSIFICATION
 
@@ -537,8 +413,8 @@ def token_to_dict(token):
         "offset": "",
         "fontSize": ""
     }
-    converted_token['text'] = span.text
-    converted_token['offset'] = span.idx
+    converted_token['text'] = token.text
+    converted_token['offset'] = token.idx
     # converted_token['style']
     # converted_token['font'] = span.ent_type_
     # converted_token['fontSize'] = span.i
@@ -605,62 +481,3 @@ def get_sentence_boundaries(words, spaces):
     return sentence_offsetTokens
 
 
-def process_file(input_path):
-    input_filename = ntpath.basename(input_path)
-    extracted_data = extract_from_pdf(input_filename, input_path)
-
-    output_data = {"filename": input_path, "paragraphs": []}
-    for index, paragraph in enumerate(extracted_data):
-        if len(paragraph['spans']) > 0:
-            extracted_data_from_paragraphs = process_paragraph(paragraph)
-            if len(extracted_data_from_paragraphs) > 0:
-                output_data['paragraphs'].append(extracted_data_from_paragraphs)
-
-    return output_data
-    # processText(output[14])
-    # processText(output[17])
-
-
-if __name__ == '__main__':
-
-    if len(argv) != 3:
-        print("Invalid parameters. Usage: python linkingModule.py input output")
-        print("if input is a directory will process every PDF file recursively.")
-
-        # print("Meanwhile we are processing the default hardcoded documents :-) ")
-
-        # process_file("/Users/lfoppiano/development/superconductors/supercon_papers/supercon_files_20190308/aip/data/fy2005/JAP/97/7/10.1063_1.1871335/10.1063_1.1871335_fulltext.pdf")
-        # process_file("/Users/lfoppiano/development/superconductors/supercon_papers/supercon_files_20190308/aip/data/fy2018/JCP/v149/i7/074101_1/vor_1.5031202.pdf")
-        # process_file("/Users/lfoppiano/development/superconductors/supercon_papers/supercon_files_20190308/aip/data/fy2005/JAP/98/3/10.1063_1.1997288/10.1063_1.1997288_fulltext.pdf")
-        # process_file("/Users/lfoppiano/development/superconductors/supercon_papers/supercon_files_20190308/aps/data/fy2018/PRB/2017/PhysRevB.95.224501/online.pdf")
-        # process_dir(
-        #     "/Users/lfoppiano/development/superconductors/supercon_papers/supercon_files_20190308/aip/data/fy2018/JCP/v148/i19/194701_1/",
-        #     "output")
-        # process_dir(
-        #     "/Users/lfoppiano/development/superconductors/supercon_papers/supercon_files_20190308/aip/data/fy2018/JAP/v123/i5/053301_1/",
-        #     "output")
-        # process_dir(
-        #     "/Users/lfoppiano/development/superconductors/supercon_papers/supercon_files_20190308/aip/data/fy2018/JAP/v123/i5/053301_1/",
-        #     "output")
-        sys.exit(-1)
-
-    input = argv[1]
-    output = argv[2]
-
-    if os.path.isdir(input):
-        input_path = Path(input)
-        output_path = Path(output)
-        if os.path.isdir(output):
-            output_path = Path(output)
-
-        process_dir(input_path, output_path)
-
-    elif os.path.isfile(input):
-        input_path = Path(input)
-        output_data = process_file(str(input_path))
-        with open(Path(output + '/output_single.json'), 'w') as f:
-            f.write(json.dumps(output_data) + "\n")
-
-    # for token in doc:
-    #     print(token.text, token.dep_, token.head.text, token.head.pos_,
-    #           [child for child in token.children])
