832582bae8598b28cb2722533cfb8b17ae468132
Luca Foppiano <luca@foppiano.org>
2020-03-16 16:56:02 +0900

    adding first implementation of copy-data to clipboard

--- src/static/grobid/grobid-superconductors.js
+++ src/static/grobid/grobid-superconductors.js
@@ -43,11 +43,89 @@ var grobid = (function ($) {
             }
         }
 
+        function copyTextToClipboard(text) {
+            var textArea = document.createElement("textarea");
+
+            //
+            // *** This styling is an extra step which is likely not required. ***
+            //
+            // Why is it here? To ensure:
+            // 1. the element is able to have focus and selection.
+            // 2. if element was to flash render it has minimal visual impact.
+            // 3. less flakyness with selection and copying which **might** occur if
+            //    the textarea element is not visible.
+            //
+            // The likelihood is the element won't even render, not even a
+            // flash, so some of these are just precautions. However in
+            // Internet Explorer the element is visible whilst the popup
+            // box asking the user for permission for the web page to
+            // copy to the clipboard.
+            //
+
+            // Place in top-left corner of screen regardless of scroll position.
+            textArea.style.position = 'fixed';
+            textArea.style.top = 0;
+            textArea.style.left = 0;
+
+            // Ensure it has a small width and height. Setting to 1px / 1em
+            // doesn't work as this gives a negative w/h on some browsers.
+            textArea.style.width = '2em';
+            textArea.style.height = '2em';
+
+            // We don't need padding, reducing the size if it does flash render.
+            textArea.style.padding = 0;
+
+            // Clean up any borders.
+            textArea.style.border = 'none';
+            textArea.style.outline = 'none';
+            textArea.style.boxShadow = 'none';
+
+            // Avoid flash of white box if rendered for any reason.
+            textArea.style.background = 'transparent';
+
+
+            textArea.value = text;
+
+            document.body.appendChild(textArea);
+            textArea.focus();
+            textArea.select();
+
+            try {
+                var successful = document.execCommand('copy');
+                var msg = successful ? 'successful' : 'unsuccessful';
+                console.log('Copying text command was ' + msg);
+            } catch (err) {
+                console.log('Oops, unable to copy');
+            }
+
+            document.body.removeChild(textArea);
+        }
+
+        function copyOnClipboard() {
+            console.log("Copying data on clipboard! ");
+            var tableResultsBody = $('#tableResultsBody');
+
+            var textToBeCopied = "";
+
+            var rows = tableResultsBody.find("tr");
+            $.each(rows, function () {
+                var tds = $(this).children();
+                var material = tds[1].textContent;
+                var tc = tds[2].textContent;
+
+                textToBeCopied += material + "\t" + tc + "\n";
+            });
+            copyTextToClipboard(textToBeCopied);
+
+        }
+
         $(document).ready(function () {
             $("#divRestI").show();
             $('#tableResults').hide();
             configuration = load_configuration();
             $('#submitRequest').bind('click', 'processPDF', submitQuery);
+            $('#copy-button').bind('click', copyOnClipboard);
+            $('#copy-button').hide();
 
             //turn to inline mode
             $.fn.editable.defaults.mode = 'inline';
@@ -245,6 +323,9 @@ var grobid = (function ($) {
 
             var spanGlobalIndex = 0;
             var linkId = 0;
+
+            var copyButtonElement = $('#copy-button');
+
             paragraphs.forEach(function (paragraph, paragraphIdx) {
                 var spans = paragraph.spans;
                 // hey bro, this must be asynchronous to avoid blocking the brothers
@@ -309,6 +390,7 @@ var grobid = (function ($) {
 
                             linkId++;
                         });
+                        copyButtonElement.show();
                     }
                 })
             });
