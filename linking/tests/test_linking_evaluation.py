7febac8c2dac39ae176bc427b448beaae42db9df
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2020-08-06 09:43:18 +0900

    implementing text processing and extending the linking evaluation to the crf linker

--- linking/tests/test_linking_evaluation.py
+++ linking/tests/test_linking_evaluation.py
@@ -1,6 +1,6 @@
 import logging
 
-from linking_evaluation import compute_metrics, compute_metrics_by_type, get_report
+from linking_evaluation import compute_metrics_by_type, tokenize_chunk
 from linking_module import MATERIAL_TC_TYPE, TC_PRESSURE_TYPE
 
 LOGGER = logging.getLogger(__name__)
@@ -16,3 +16,11 @@ class TestLinkingModule:
         assert output['precision'] == 0.6666666666666666
         assert output['recall'] == 1.0
         assert output['f1'] == 0.8
+
+    def test_tokenize_chunk(self):
+        text = "This is a text I want to tokenize."
+        offset = 0
+        tokenized_chunks, new_offset = tokenize_chunk(text, offset)
+
+        assert len(tokenized_chunks) == 16
+        assert tokenized_chunks[2]['offset'] == 5
