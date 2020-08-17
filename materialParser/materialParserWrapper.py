32a758fd2762944b5c9e55fc1c74000a4c2db796
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2020-08-17 16:46:11 +0900

    resolving various merges conflicts

--- materialParser/materialParserWrapper.py
+++ materialParser/materialParserWrapper.py
@@ -1,28 +1,28 @@
-from material_parser.material_parser import MaterialParser
+# from material_parser.material_parser import MaterialParser
 
 from material2class import Material2Class
 
 
 class MaterialParserWrapper:
 
-    mp = MaterialParser()
+    # mp = MaterialParser()
     material2class = Material2Class()
 
-    def formula_to_composition(self, formula):
-        composition = self.mp.formula2composition(formula)
-
-        if 'elements' in composition:
-            return composition
-
-        else:
-            return {}
-
+    # def formula_to_composition(self, formula):
+    #     composition = self.mp.formula2composition(formula)
+    #
+    #     if 'elements' in composition:
+    #         return composition
+    #
+    #     else:
+    #         return {}
+    #
     def formula_to_class(self, formula):
         clazz = self.material2class.get_class(formula)
 
         return clazz
 
-    def name_to_formula(self, name):
-        formula = self.mp.string2formula(name)
-
-        return formula
+    # def name_to_formula(self, name):
+    #     formula = self.mp.string2formula(name)
+    #
+    #     return formula
