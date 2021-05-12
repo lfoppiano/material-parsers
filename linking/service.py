651ef1097b83e63e597333b8c11fe77d3107db03
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2021-05-12 11:35:12 +0900

    simplifying service

--- linking/service.py
+++ linking/service.py
@@ -9,68 +9,65 @@ from materialParserWrapper import MaterialParserWrapper
 
 bottle.BaseRequest.MEMFILE_MAX = 1024 * 1024 * 1024
 
+linker_material_tc = RuleBasedLinker(source="<tcValue>", destination="<material>")
+linker_tc_pressure = RuleBasedLinker(source="<pressure>", destination="<tcValue>")
+temperature_classifier = CriticalTemperatureClassifier()
 
-class Service(object):
-    def __init__(self):
-        self.linker_material_tc = RuleBasedLinker(source="<tcValue>", destination="<material>")
-        self.linker_tc_pressure = RuleBasedLinker(source="<pressure>", destination="<tcValue>")
-        self.temperature_classifier = CriticalTemperatureClassifier()
+@route('/info')
+def info():
+    returnText = "Python utilities wrapper as a micro-service."
+    return returnText
 
-    @route('/info')
-    def info(self):
-        returnText = "Python utilities wrapper as a micro-service."
-        return returnText
+@route('/process/tc', method="POST")
+def mark_critical_temperature():
+    input_raw = request.forms.get("input")
 
-    @route('/process/tc', method="POST")
-    def mark_critical_temperature(self):
-        input_raw = request.forms.get("input")
+    return temperature_classifier.mark_temperatures_paragraph_json(input_raw)
 
-        return self.temperature_classifier.mark_temperatures_paragraph_json(input_raw)
+@route('/process/links', method="POST")
+def create_links():
+    input_raw = request.forms.get("input")
+    paragraph_input = json.loads(input_raw)
 
-    @route('/process/links', method="POST")
-    def create_links(self):
-        input_raw = request.forms.get("input")
-        paragraph_input = json.loads(input_raw)
+    material_tc_linked = linker_material_tc.process_paragraph(paragraph_input)
+    tc_pressure_linked = linker_tc_pressure.process_paragraph(paragraph_input)
 
-        material_tc_linked = self.linker_material_tc.process_paragraph(paragraph_input)
-        tc_pressure_linked = self.linker_tc_pressure.process_paragraph(paragraph_input)
+    spans_map = {}
+    for paragraphs in tc_pressure_linked:
+        for span in paragraphs['spans'] if 'spans' in paragraphs else []:
+            if 'links' in span and span['id'] in spans_map and 'links' in spans_map[span['id']] \
+                and len(list(filter(lambda w: w['type'] != "crf", spans_map[span['id']]['links']))) > 0:
+                spans_map[span['id']] = span
 
-        spans_map = {}
-        for paragraphs in tc_pressure_linked:
-            for span in paragraphs['spans'] if 'spans' in paragraphs else []:
-                if 'links' in span and span['id'] in spans_map and 'links' in spans_map[span['id']] \
-                    and len(list(filter(lambda w: w['type'] != "crf", spans_map[span['id']]['links']))) > 0:
-                    spans_map[span['id']] = span
+    for paragraphs in material_tc_linked:
+        for span in paragraphs['spans'] if 'spans' in paragraphs else []:
+            if span['id'] in spans_map and 'links' in spans_map[span['id']]:
+                links_list = list(filter(lambda w: w['type'] != "crf", spans_map[span['id']]['links']))
+                if 'links' in span:
+                    span['links'].extend(links_list)
+                else:
+                    span['links'] = links_list
 
-        for paragraphs in material_tc_linked:
-            for span in paragraphs['spans'] if 'spans' in paragraphs else []:
-                if span['id'] in spans_map and 'links' in spans_map[span['id']]:
-                    links_list = list(filter(lambda w: w['type'] != "crf", spans_map[span['id']]['links']))
-                    if 'links' in span:
-                        span['links'].extend(links_list)
-                    else:
-                        span['links'] = links_list
+    # for paragraphs in material_tc_linked:
+    #     material_tc_linked['relationships'].extends(tc_pressure_linked['relationships'])
 
-        # for paragraphs in material_tc_linked:
-        #     material_tc_linked['relationships'].extends(tc_pressure_linked['relationships'])
+    return json.dumps(material_tc_linked)
 
-        return json.dumps(material_tc_linked)
+@route('/process/formula', method="POST")
+def resolve_class():
+    formula_raw = request.forms.get("input")
+    classes = MaterialParserWrapper().formula_to_classes(formula_raw)
 
-    @route('/process/formula', method="POST")
-    def resolve_class(self):
-        formula_raw = request.forms.get("input")
-        classes = MaterialParserWrapper().formula_to_classes(formula_raw)
+    return json.dumps(list(classes.keys()))
 
-        return json.dumps(list(classes.keys()))
+@route('/process/all', method="POST")
+def process():
+    input_raw = request.forms.get("input")
+    input_json = json.loads(input_raw)
+    paragraph_with_marked_tc = linker_material_tc.mark_temperatures_paragraph(input_json)
 
-    @route('/process/all', method="POST")
-    def process(self):
-        input_raw = request.forms.get("input")
-        input_json = json.loads(input_raw)
-        paragraph_with_marked_tc = self.linker_material_tc.mark_temperatures_paragraph(input_json)
-
-        material_tc_linked = self.linker_material_tc.process_paragraph_json(paragraph_with_marked_tc)
-        return self.linker_tc_pressure.process_paragraph_json(material_tc_linked)
+    material_tc_linked = linker_material_tc.process_paragraph_json(paragraph_with_marked_tc)
+    return linker_tc_pressure.process_paragraph_json(material_tc_linked)
 
 
 if __name__ == '__main__':
