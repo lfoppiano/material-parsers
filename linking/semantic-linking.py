d9f9dbd33c82e69002781cc55d76ac02d282b10f
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2021-08-11 17:34:40 +0900

    cleanup

--- linking/semantic-linking.py
+++
@@ -1,5 +0,0 @@
-from allennlp.predictors.predictor import Predictor
-predictor = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/bert-base-srl-2020.03.24.tar.gz")
-predictor.predict(
-  sentence="Did Uriah honestly think he could beat the game in under three hours?"
-)
