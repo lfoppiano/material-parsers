0d366c588f56b8f8e0c4598e880cee53c82dffec
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2021-09-01 11:11:27 +0900

    update dockerfile and version

--- linking/setup.py
+++ linking/setup.py
@@ -19,7 +19,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='linking',
-    version='0.0.2',
+    version='1.0',
     description='Linking Module',
     url='https://github.com/lfoppiano/grobid-superconductors-web',
     author='Luca Foppiano',
