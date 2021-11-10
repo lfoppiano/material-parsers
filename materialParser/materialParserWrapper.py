4587b7ce8abfb3dca8baa8dfec78b2a1e0641f88
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2021-11-10 15:06:37 +0900

    improve realiability of the material parser

--- materialParser/materialParserWrapper.py
+++ materialParser/materialParserWrapper.py
@@ -1,4 +1,3 @@
-from material2class import Material2Class, Material2Tags
 from sympy import SympifyError
 from text2chem.parser_pipeline import ParserPipelineBuilder
 from text2chem.postprocessing_tools.substitute_additives import SubstituteAdditives
@@ -8,23 +7,24 @@ from text2chem.preprocessing_tools.mixture_processing import MixtureProcessing
 from text2chem.preprocessing_tools.phase_processing import PhaseProcessing
 from text2chem.regex_parser import RegExParser
 
+from material2class import Material2Class, Material2Tags
+
 
 class MaterialParserWrapper:
     def __init__(self, material_parser=None):
         if not material_parser:
             material_parser = ParserPipelineBuilder() \
-            .add_preprocessing(AdditivesProcessing) \
-            .add_preprocessing(ChemicalNameProcessing) \
-            .add_preprocessing(PhaseProcessing) \
-            .add_preprocessing(MixtureProcessing) \
-            .add_postprocessing(SubstituteAdditives) \
-            .set_regex_parser(RegExParser) \
-            .build()
-        
+                .add_preprocessing(AdditivesProcessing) \
+                .add_preprocessing(ChemicalNameProcessing) \
+                .add_preprocessing(PhaseProcessing) \
+                .add_preprocessing(MixtureProcessing) \
+                .add_postprocessing(SubstituteAdditives) \
+                .set_regex_parser(RegExParser) \
+                .build()
+
         self.material_parser = material_parser
         self.material2class = Material2Class()
         self.material2tags = Material2Tags()
-        
 
     def formula_to_class(self, formula):
         return self.material2class.get_class(formula)
@@ -34,18 +34,26 @@ class MaterialParserWrapper:
 
     def formula_to_composition(self, formula):
         structured_formula = {}
+
+        output = self.parse_material(formula)
+
+        if output.composition:
+            structured_formula['composition'] = output.composition[0].elements
+
+        return structured_formula
+
+    def parse_material(self, formula):
+        """We parse something that should be a formula, therefore we might consider to replace additional characters, 
+        remove spaces and so on..."""
+        formula_without_spaces = formula.replace(" ", "")
         try:
-            output = self.material_parser.parse(formula)
+            output = self.material_parser.parse(formula_without_spaces)
         except SympifyError as e:
             raise ValueError(e)
         except ValueError as ve:
-            output = self.material_parser.parse(formula.replace(" ", ""))
+            output = self.material_parser.parse(formula_without_spaces.replace("−", "-"))
 
-        if output.composition:
-            composition_ = output.composition[0]
-            structured_formula['composition'] = composition_.elements
-                
-        return structured_formula
+        return output
 
     def name_to_formula(self, name):
         output_formula = {}
@@ -57,8 +65,7 @@ class MaterialParserWrapper:
             output = self.material_parser.parse(name.replace(" ", ""))
 
         if output.composition:
-            composition_ = output.composition[0]
-            output_formula['composition'] = composition_.elements
+            output_formula['composition'] = output.composition[0].elements
 
         output_formula['name'] = output.material_name
         output_formula['formula'] = output.material_formula
